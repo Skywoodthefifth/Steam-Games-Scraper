c08ad79b055a26f3ba7aa35ddafc99bfb4554105
Vo Huu Nam Truong <namtruong0900@gmail.com>
2024-06-02 14:57:04 +0700

    update

--- ConvertToCSV.py
+++ ConvertToCSV.py
@@ -1,24 +1,3 @@
-########################################################################################################################
-# Copyright (c) Martin Bustos @FronkonGames <fronkongames@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-# the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-########################################################################################################################
-__author__ = "Martin Bustos <fronkongames@gmail.com>"
-__copyright__ = "Copyright 2022, Martin Bustos"
-__license__ = "MIT"
-__version__ = "1.1.0"
-__email__ = "fronkongames@gmail.com"
-
 import sys
 import os
 import json
@@ -52,7 +31,7 @@ def WriteStringArray(app, key):
 def WriteKey(app, key, default = '0'):
   return str(app[key]) if key in app else default
 
-print(f'Convert JSON to CSV {__version__} by {__author__}.')
+print(f'Convert JSON to CSV')
 parser = argparse.ArgumentParser(description='Convert JSON to CSV.')
 parser.add_argument('-f', '--file', type=str, default='games.json', help='Dataset file name')
 args = parser.parse_args()
--- SteamGamesScraper.py
+++ SteamGamesScraper.py
@@ -1,24 +1,3 @@
-########################################################################################################################
-# Copyright (c) Martin Bustos @FronkonGames <fronkongames@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-# the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-########################################################################################################################
-__author__ = "Martin Bustos <fronkongames@gmail.com>"
-__copyright__ = "Copyright 2022, Martin Bustos"
-__license__ = "MIT"
-__version__ = "1.2.2"
-__email__ = "fronkongames@gmail.com"
-
 import sys
 import os
 import re
@@ -452,7 +431,7 @@ def UpdateFromCSV(dataset, notreleased, discarted, args):
     Log(ERROR, f'File {args.update} not found')
 
 if __name__ == "__main__":
-  Log(INFO, f'Steam Games Scraper {__version__} by {__author__}')
+  Log(INFO, f'Steam Games Scraper')
   parser = argparse.ArgumentParser(description='Steam games scraper.')
   parser.add_argument('-i', '--infile',   type=str,   default=DEFAULT_INFILE,  help='Input file name')
   parser.add_argument('-o', '--outfile',  type=str,   default=DEFAULT_OUTFILE,  help='Output file name')
